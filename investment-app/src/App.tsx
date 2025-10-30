9e43be3a426f1bfcf834ea720f8fdcdc26e6fa78
Stasiek99 <stasiekpilich@gmail.com>
2025-10-30 11:23:02 +0100

    c

--- investment-app/src/App.tsx
+++ investment-app/src/App.tsx
@@ -1,36 +1,7 @@
-import { useState } from "react";
-
-import Header from "./components/Header.tsx";
-import UserInput from "./components/UserInput.tsx";
-import Results from "./components/Results.tsx";
+import {InvestmentsCalculatorPage} from "../features/investments-calculator";
 
 function App() {
-    const [userInput, setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10
-    });
-
-    const inputIsValid = userInput.duration >= 1;
-
-    function handleChange(inputIdentifier: string, newValue: string) {
-        setUserInput(prevUserInput => {
-            return {
-                ...prevUserInput,
-                [inputIdentifier]: +newValue
-            };
-        });
-    }
-
-    return (
-        <>
-            <Header></Header>
-            <UserInput userInput={userInput} onChange={handleChange}></UserInput>
-            {!inputIsValid && <p className="center">Please enter a duration greater than zero</p>}
-            {inputIsValid && <Results input={userInput}></Results>}
-        </>
-    )
+    return <InvestmentsCalculatorPage />;
 }
 
-export default App
+export default App;
