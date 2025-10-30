5858d30146b5e1d850a6037d0c83a332263af312
Stasiek99 <stasiekpilich@gmail.com>
2025-10-30 12:10:51 +0100

    lifted state up

--- investment-app/src/App.tsx
+++ investment-app/src/App.tsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+
 import Header from "./components/Header.tsx";
 import UserInput from "./components/UserInput.tsx";
+import Results from "./components/Results.tsx";
 
 function App() {
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    });
+
+    function handleChange(inputIdentifier: string, newValue: string) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue
+            };
+        });
+    }
+
     return (
         <>
             <Header></Header>
-            <UserInput></UserInput>
+            <UserInput userInput={userInput} onChange={handleChange}></UserInput>
+            <Results input={userInput}></Results>
         </>
     )
 }
