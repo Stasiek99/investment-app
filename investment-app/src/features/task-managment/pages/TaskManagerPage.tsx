3abaafbba11c9089565b1ad6dd5f07b2b47ee09f
Stasiek99 <stasiekpilich@gmail.com>
2025-11-03 09:12:23 +0100

    feature: added ProjectsSidebar component

--- investment-app/src/features/task-managment/pages/TaskManagerPage.tsx
+++ investment-app/src/features/task-managment/pages/TaskManagerPage.tsx
@@ -1,9 +1,13 @@
+import ProjectsSidebar from "../components/ProjectsSidebar.tsx";
+
 export default function TaskManagerPage() {
     return (
         <div className="p-8">
             <h2 className="text-3xl font-bold mb-4">Task Management</h2>
             <p className="text-grey-600">
-                Here you’ll be able to create and manage tasks related to your investment projects.
+                <main className="h-screen my-8">
+                    <ProjectsSidebar />
+                </main>
             </p>
         </div>
     );
