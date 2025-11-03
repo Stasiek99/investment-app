220d6f41ffbab88413041688713c0645d2f2dabb
Stasiek99 <stasiekpilich@gmail.com>
2025-11-03 10:07:11 +0100

    feature: added NewProject and Input components

--- investment-app/src/app/layout/MainLayout.tsx
+++ investment-app/src/app/layout/MainLayout.tsx
@@ -2,16 +2,24 @@ import { Outlet, NavLink } from "react-router-dom";
 
 export default function MainLayout() {
     return (
-        <div className="min-h-screen bg-grey-50 text-gray-800">
+        <div className="min-h-screen bg-gray-50 text-gray-800">
             <header className="bg-blue-600 text-white py-4 px-6 flex justify-between items-center">
                 <h1 className="text-2xl font-bold">Investment Tools</h1>
                 <nav className="space-x-4">
-                    <NavLink to="/"
-                             className={({ isActive }) => `hover:underline ${isActive ? "font-bold underline" : ""}`}>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            `hover:underline ${isActive ? "font-bold underline" : ""}`
+                        }
+                    >
                         Investments
                     </NavLink>
-                    <NavLink to="/tasks"
-                             className={({ isActive }) => `hover:underline ${isActive ? "font-bold underline" : ""}`}>
+                    <NavLink
+                        to="/tasks"
+                        className={({ isActive }) =>
+                            `hover:underline ${isActive ? "font-bold underline" : ""}`
+                        }
+                    >
                         Tasks
                     </NavLink>
                 </nav>
--- investment-app/src/features/task-managment/pages/TaskManagerPage.tsx
+++ investment-app/src/features/task-managment/pages/TaskManagerPage.tsx
@@ -1,14 +1,11 @@
 import ProjectsSidebar from "../components/ProjectsSidebar.tsx";
+import NewProject from "../components/NewProject.tsx";
 
 export default function TaskManagerPage() {
     return (
-        <div className="p-8">
-            <h2 className="text-3xl font-bold mb-4">Task Management</h2>
-            <p className="text-grey-600">
-                <main className="h-screen my-8">
-                    <ProjectsSidebar />
-                </main>
-            </p>
-        </div>
+        <main className="h-screen my-8 flex gap-8">
+            <ProjectsSidebar />
+            <NewProject />
+        </main>
     );
 }
