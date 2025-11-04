b762e487f03fc86e0610225d56a4034f0ba5d7c1
Stasiek99 <stasiekpilich@gmail.com>
2025-11-04 19:02:34 +0100

    feature: added NoProjectsSelected component

--- investment-app/src/features/investments-calculator/pages/InvestmentsCalculatorPage.tsx
+++ investment-app/src/features/investments-calculator/pages/InvestmentsCalculatorPage.tsx
@@ -1,19 +1,34 @@
-import {useInvestmentInput} from "../hooks/useInvestmentInput.ts";
+import { useEffect } from "react";
+
 import Header from "../components/Header.tsx";
 import UserInput from "../components/UserInput.tsx";
 import Results from "../components/Results.tsx";
+import { useInvestmentInput } from "../hooks/useInvestmentInput.ts";
 
 export default function InvestmentsCalculatorPage() {
     const {userInput, handleChange, inputIsValid} = useInvestmentInput();
 
+    useEffect(() => {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = new URL("../styles.css", import.meta.url).href;
+        document.head.appendChild(link);
+
+        return () => {
+            document.head.removeChild(link);
+            document.body.removeAttribute("style");
+            document.body.className = "";
+        };
+    }, []);
+
     return (
-        <>
+        <div className="investment-app">
             <Header />
             <UserInput userInput={userInput} onChange={handleChange} />
             {!inputIsValid && (
                 <p className="center">Please enter a duration greater than zero</p>
             )}
             {inputIsValid && <Results input={userInput} />}
-        </>
+        </div>
     );
 }
--- investment-app/src/features/task-managment/pages/TaskManagerPage.tsx
+++ investment-app/src/features/task-managment/pages/TaskManagerPage.tsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+
 import ProjectsSidebar from "../components/ProjectsSidebar.tsx";
 import NewProject from "../components/NewProject.tsx";
+import NoProjectsSelected from "../components/NoProjectsSelected.tsx";
 
 export default function TaskManagerPage() {
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: [],
+    });
+
+    function handleStartAddProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null
+            };
+        });
+    }
+
+    let content;
+
+    if (projectsState.selectedProjectId === null) {
+        content = <NewProject />;
+    } else if (projectsState.selectedProjectId === undefined) {
+        content = <NoProjectsSelected />;
+    }
+
     return (
         <main className="h-screen my-8 flex gap-8">
-            <ProjectsSidebar />
-            <NewProject />
+            <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+            {content}
         </main>
     );
 }
